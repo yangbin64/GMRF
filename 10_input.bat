2a96fbc896005891832b8823037352fc406bb666
yangbin64 <36215838+yangbin64@users.noreply.github.com>
2018-05-07 16:59:07 +0900

    Add files via upload

--- 10_input.bat
+++ 10_input.bat
@@ -1,6 +1,8 @@
-head -n 121 ../10_data/model.csv | cut -f3- -d, > input2.csv
+rem head -n 121 ../10_data/model.csv | cut -f3- -d, > input2.csv
+head -n 7381 ../10_data/model.csv | tail -n 121 | cut -f3- -d, > input2.csv
 
-head -n 1331 ../10_data/model.csv | cut -f2- -d, > input3.csv
+rem head -n 1331 ../10_data/model.csv | cut -f2- -d, > input3.csv
+head -n 7986 ../10_data/model.csv | tail -n 1331 | cut -f2- -d, > input3.csv
 
 cp ../10_data/model.csv input4.csv
 
